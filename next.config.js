e8100e9a3d4d35beb106736ea87a7f033cb574bf
Juan-sanche <155849385+Juan-sanche@users.noreply.github.com>
2025-07-21 10:12:09 +0200

    Fix NextAuth build issue for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove the deprecated appDir
-  experimental: {
-    // Remove appDir as it's default in Next.js 14
-  },
-  
-  // Add this to fix NextAuth build issue
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@next-auth/prisma-adapter')
-    }
-    return config
-  },
-  
-  // Exclude NextAuth routes from static generation
-  async generateBuildId() {
-    return 'build'
-  }
+  // Remove experimental.appDir as it's deprecated
 }
 
 module.exports = nextConfig
